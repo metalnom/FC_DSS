cc37e2b5a3a9dfd1655b859244f8b7adf924aebf
metalnom <jcd4625@gmail.com>
2020-04-23 18:07:27 +0900

    07

---
+++ 01_Python/school/dss/data1.py
@@ -0,0 +1,4 @@
+
+def plus1(*args):
+    print("data1")
+    return sum(args)
---
+++ 01_Python/school/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dss",
+    packages=find_packages(),
+    include_package_data=True,
+    version="0.0.1",
+    author="JCD",
+    author_email="jcd4625@gmail.com",
+    zip_safe=False,
+)
